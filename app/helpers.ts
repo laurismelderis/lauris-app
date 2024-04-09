a08b65faaba0a5d493863a901ab59937f1e7822c
laurismelderis <lauris.melderis77@gmail.com>
2024-04-09 21:56:35 +0300

    patch: fix test

--- app/helpers.ts
+++ app/helpers.ts
@@ -1,5 +1,8 @@
 const datePostfixes = ['st', 'nd', 'rd', 'th']
 
 export const getDatePostfix = (day: number | undefined) => {
+  if (day && day > 10 && day < 20) {
+    return datePostfixes[3]
+  }
   return day ? datePostfixes[(day - 1) % 10 < 4 ? (day - 1) % 10 : 3] : ''
 }
