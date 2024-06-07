23b3d8796b6b9120e85ca35acf6b17300bad8dd4
laurismelderis <lauris.melderis77@gmail.com>
2024-06-07 22:32:36 +0300

    patch: test around

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,16 +1,16 @@
-import React, { Suspense } from 'react'
-import EventList from '../../components/cv/EventList'
+import React from 'react'
+// import EventList from '../../components/cv/EventList'
 import AddEvent from '@/src/components/cv/AddEvent'
-import Loading from './loading'
+// import Loading from './loading'
 
 const Cv = () => {
   return (
     <div className='pt-8'>
       <div className='mx-auto w-4/6 relative flex flex-col gap-4'>
-        <Suspense fallback={<Loading />}>
+        {/* <Suspense fallback={<Loading />}>
           <EventList />
-          <AddEvent />
-        </Suspense>
+        </Suspense> */}
+        <AddEvent />
       </div>
     </div>
   )
