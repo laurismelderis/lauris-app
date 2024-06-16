70d79d0cf71761fc06f83df62c9a6396c7449d4f
laurismelderis <lauris.melderis77@gmail.com>
2024-06-16 14:46:28 +0300

    patch: update view height measurements to dynamic

--- src/components/Unauthorized.tsx
+++ src/components/Unauthorized.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Unauthorized = () => {
   return (
-    <div className='flex flex-col justify-center items-center text-green min-h-[calc(100vh-100px)]'>
+    <div className='flex flex-col justify-center items-center text-green min-h-[calc(100dvh-100px)]'>
       Unauthorized
     </div>
   )
