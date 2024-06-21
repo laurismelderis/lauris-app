68a5e5a53b140f6492c40917c8521276a93ae170
laurismelderis <lauris.melderis77@gmail.com>
2024-06-21 17:29:32 +0300

    feat: add social media links

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,1 +1,13 @@
+import { CSSProperties } from 'react'
+
+export type IconProps = {
+  width?: string
+  height?: string
+  style?: CSSProperties
+  className?: React.ComponentProps<'svg'>['className']
+}
+
+export { default as IconFacebook } from './IconFacebook'
+export { default as IconInstagram } from './IconInstagram'
+export { default as IconLinkedin } from './IconLinkedin'
 export { default as IconPen } from './IconPen'
