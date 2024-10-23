0b742ec2c1880070398d58bccd59ee384d5a9709
laurismelderis <lauris.melderis77@gmail.com>
2024-10-23 14:30:15 +0300

    Improve seo

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,19 @@
-import { MetadataRoute } from 'next'
+import { getEvents } from '../libs/cv'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://laurism.me'
+export default async function sitemap() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
+  const events = await getEvents()
+
+  const eventSlugs = events
+    .filter((event) => !event.isDraft)
+    .map((event) => ({
+      url: `${baseUrl}/cv/${event.slug}`,
+      lastModified: event.lastModified,
+    }))
 
   return [
+    ...eventSlugs,
     { url: baseUrl, lastModified: new Date() },
     { url: `${baseUrl}/cv`, lastModified: new Date() },
   ]
